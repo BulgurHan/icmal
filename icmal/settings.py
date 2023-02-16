f127f1ea96a4cdcc1300742a932c714134e05def
BulgurHan <mertgurkan034@gmail.com>
2023-02-16 17:18:19 +0300

    root ile girmeyi deniyorum

--- icmal/settings.py
+++ icmal/settings.py
@@ -80,8 +80,8 @@ WSGI_APPLICATION = 'icmal.wsgi.application'
 DATABASES = {
       'default': {
           'ENGINE': 'django.db.backends.mysql',
-          'NAME': 'default',
-          'USER': 'kocak',
+          'NAME': 'icmal',
+          'USER': 'root',
            'PASSWORD': 'mert3446007',
             'HOST': 'kocak.mysql.pythonanywhere-services.com',
            'PORT': '3306',
