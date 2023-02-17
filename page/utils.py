351f9064a95c288dd754da2e28e92c33ac9e5148
BulgurHan <mertgurkan034@gmail.com>
2023-02-17 15:58:54 +0300

    static root değişti

--- page/utils.py
+++ page/utils.py
@@ -62,18 +62,17 @@ def link_callback(uri, rel):
     """
     result = finders.find(uri)
     if result:
+        print(result)
         if not isinstance(result, (list, tuple)):
             result = [result]
         result = list(os.path.realpath(path) for path in result)
         path = result[0]
     else:
+        
         sUrl = settings.STATIC_URL        # Typically /static/
-        sRoot = settings.STATIC_ROOT      # Typically /home/userX/project_static/
-        mUrl = settings.MEDIA_URL         # Typically /media/
-        mRoot = settings.MEDIA_ROOT       # Typically /home/userX/project_static/media/
-        if uri.startswith(mUrl):
-            path = os.path.join(mRoot, uri.replace(mUrl, ""))
-        elif uri.startswith(sUrl):
+        sRoot = settings.STATIC_ROOT     # Typically /home/userX/project_static/
+        
+        if uri.startswith(sUrl):
             path = os.path.join(sRoot, uri.replace(sUrl, ""))
         else:
             return uri
